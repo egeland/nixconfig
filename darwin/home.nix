a6df16cb8a5212457d22a1a5c9f9894cc0e3de43
Frode Egeland <egeland@gmail.com>
2022-11-30 23:16:19 +1100

    add git aliases
    Signed-off-by: Frode Egeland <egeland@gmail.com>

---
+++ darwin/home.nix
@@ -0,0 +1,131 @@
+#
+#  Home-manager configuration for macbook
+#
+#  flake.nix
+#   ├─ ./darwin
+#   │   ├─ ./default.nix
+#   │   └─ ./home.nix *
+#   └─ ./modules
+#       └─ ./programs
+#           └─ ./alacritty.nix
+#
+{pkgs, ...}: {
+  # imports =
+  #   [
+  #     ../modules/programs/alacritty.nix
+  #   ];
+
+  home = {
+    # Specific packages for macbook
+    packages = with pkgs; [
+      # Terminal
+      pfetch
+      starship
+      fish
+      lsd
+      bat
+      kitty
+    ];
+    stateVersion = "22.05";
+  };
+
+  programs = {
+    vscode = {
+      enable = true;
+      package = pkgs.vscode;
+      enableExtensionUpdateCheck = true;
+      extensions = with pkgs; [
+        vscode-extensions.bbenoist.nix
+        vscode-extensions.kamadorueda.alejandra
+        vscode-extensions.ms-python.python
+        vscode-extensions.mhutchie.git-graph
+        vscode-extensions.timonwong.shellcheck
+        vscode-extensions.oderwat.indent-rainbow
+        vscode-extensions.donjayamanne.githistory
+        vscode-extensions.yzhang.markdown-all-in-one
+      ];
+    };
+    git = {
+      enable = true;
+      package = pkgs.git;
+      userName = "Frode Egeland";
+      userEmail = "egeland@gmail.com";
+      delta.enable = true;
+      aliases = {
+        ci = "commit --signoff";
+        prp = "pull --rebase --prune";
+        lg = "log --oneline --abbrev-commit --all --graph --decorate --color --show-signature --no-merges";
+        cb = "checkout -b";
+        co = "checkout";
+      };
+      extraConfig = {
+        init = {
+          defaultBranch = "main";
+        };
+      };
+    };
+    zsh = {
+      # Post installation script is run in configuration.nix to make it default shell
+      enable = true;
+      enableAutosuggestions = true; # Auto suggest options and highlights syntax. It searches in history for options
+      enableSyntaxHighlighting = true;
+      history.size = 10000;
+
+      oh-my-zsh = {
+        # Extra plugins for zsh
+        enable = true;
+        plugins = ["git"];
+        custom = "$HOME/.config/zsh_nix/custom";
+      };
+
+      initExtra = ''
+        # Spaceship
+        source ${pkgs.spaceship-prompt}/share/zsh/site-functions/prompt_spaceship_setup
+        autoload -U promptinit; promptinit
+        pfetch
+      ''; # Zsh theme
+    };
+    neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+
+      plugins = with pkgs.vimPlugins; [
+        # Syntax
+        vim-nix
+        vim-markdown
+
+        # Quality of life
+        vim-lastplace # Opens document where you left it
+        auto-pairs # Print double quotes/brackets/etc.
+        vim-gitgutter # See uncommitted changes of file :GitGutterEnable
+
+        # File Tree
+        nerdtree # File Manager - set in extraConfig to F6
+
+        # Customization
+        wombat256-vim # Color scheme for lightline
+        srcery-vim # Color scheme for text
+
+        lightline-vim # Info bar at bottom
+        indent-blankline-nvim # Indentation lines
+      ];
+
+      extraConfig = ''
+        syntax enable                             " Syntax highlighting
+        colorscheme srcery                        " Color scheme text
+
+        let g:lightline = {
+          \ 'colorscheme': 'wombat',
+          \ }                                     " Color scheme lightline
+
+        highlight Comment cterm=italic gui=italic " Comments become italic
+        hi Normal guibg=NONE ctermbg=NONE         " Remove background, better for personal theme
+
+        set number                                " Set numbers
+
+        nmap <F6> :NERDTreeToggle<CR>             " F6 opens NERDTree
+      '';
+    };
+  };
+}
